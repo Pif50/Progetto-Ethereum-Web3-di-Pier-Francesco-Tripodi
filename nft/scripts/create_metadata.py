3ad24a0956f84ec3ff2c27d8b69292d4f815bdbd
Pierfrancesco <94386461+Pif50@users.noreply.github.com>
2022-09-27 17:35:34 +0200

    Ipfs

--- nft/scripts/create_metadata.py
+++ nft/scripts/create_metadata.py
@@ -32,7 +32,7 @@ def upload_to_ipfs(filepath):
     with Path(filepath).open("rb") as fp:
         image_binary = fp.read()
         ipfs_url = "http://127.0.0.1:5001"
-        endpoint = "api/v0/add"
+        endpoint = "/api/v0/add"
         response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
         ipfs_hash = response.json()["Hash"]
 
